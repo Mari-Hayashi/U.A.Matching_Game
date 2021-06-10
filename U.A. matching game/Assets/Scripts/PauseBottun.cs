cd25003223f3bd476e7403640d5bad1d030e52aa
Mari-Hayashi <mhaya010@ucr.edu>
2021-06-10 15:27:35 -0700

    Modified button.

--- U.A. matching game/Assets/Scripts/PauseBottun.cs
+++ U.A. matching game/Assets/Scripts/PauseBottun.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseBottun : MonoBehaviour {
 	public Sprite buttononmouse;
